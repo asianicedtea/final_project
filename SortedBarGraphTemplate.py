d9c22f70671a846df1084bbd6c033b7baba650d5
asianicedtea <kaitlyn.h.tran@gmail.com>
2025-12-07 12:32:54 -0800

    Add files via upload

---
+++ SortedBarGraphTemplate.py
@@ -0,0 +1,57 @@
+from turtle import *
+
+def drawBar(height):
+    #draw one bar of the specified height
+    #turtle will start where the last bar left off
+    return
+
+def sort(a):
+    return
+
+def readData():
+    for my
+    
+    #read the data into myList
+    #Use open() and readlines()
+    #Convert each element into an int
+    #One way to do it is to loop through and assign myList[i] = int(myList[i])
+
+    return myList
+
+def main():
+    myList = []
+    file_name = input()
+    lines = file_name.readlines()
+    i = 0
+    for i in lines:
+        myList.append(lines[i])
+    for myList[i] in range(len(myList)):
+        print(myList[i])
+    readData()
+    #get data list from readData()
+
+    #print the list of data to the console window
+
+    #find maxheight
+    #find numbars
+
+    screen = Screen()
+    #screen.setworldcoordinates(?,?,?,?)
+
+    speed('fastest')
+    
+    #draw a bar for each element in the data list
+
+    #sort the list
+
+    #print the sorted list to the console window
+    
+    reset()
+    speed('fastest')
+
+    #draw a bar for each element in the data list (again)
+
+    screen.exitonclick()
+
+if __name__ == "__main__":
+    main()
